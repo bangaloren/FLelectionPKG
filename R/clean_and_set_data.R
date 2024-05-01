259453d2ff2a19cb370df0d05d27416453420f0f
Neha Bangalore <nehabangalore@Nehas-MacBook-Pro.local>
2024-04-30 20:33:51 -0400

    edit descriptions

--- R/clean_and_set_data.R
+++ R/clean_and_set_data.R
@@ -1,6 +1,6 @@
 #' Clean and Set all Data
 #'
-#' @return nothing. just sets the data and the sata is broken up into years. set_2017 up to set_2023
+#' @return nothing. just sets the data and the sata is broken up into years. set_2017 up to set_2023. Each data set is also given a new column with county totals.
 #' @export
 #'
 #' @examples clean_and_set_data(User/R/fullVRreport.xlsx)
